9adf85769b82e60ce3cfa2c048032d8b21332099
fkunstner <frederik.kunstner@gmail.com>
2022-12-02 01:26:19 -0800

    fix relative path in kmerexpr

--- src/solver_comparison/kmerexpr.py
+++ src/solver_comparison/kmerexpr.py
@@ -4,4 +4,4 @@ from pathlib import Path
 def kmerexpr_data_path():
     from kmerexpr import sample_genome_data
 
-    return Path(sample_genome_data.__file__).parents[0] / "data"
+    return Path(sample_genome_data.__file__).parents[1] / "data"
