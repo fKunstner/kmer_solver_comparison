8ca22a4f4fac712a9cae0287887f4b85cc6473de
fkunstner <frederik.kunstner@gmail.com>
2022-12-02 20:47:04 -0800

    Add reimplementation of losses

---
+++ src/solver_comparison/problem/losses.py
@@ -0,0 +1,68 @@
+import numpy as np
+from kmerexpr.multinomial_model import multinomial_model
+from kmerexpr.multinomial_simplex_model import multinomial_simplex_model
+from numpy.typing import NDArray
+from scipy.special import softmax
+
+
+class Loss:
+    pass
+
+
+class MultinomialSimplexLoss(Loss):
+    def __init__(self, kmer_model: multinomial_simplex_model):
+        self._model = kmer_model
+        if self._model.beta != 1.0:
+            raise ValueError("Priors (using beta != 1.0) not yet implemented")
+
+    def _func(self, psi_probabilities: NDArray):
+        return self._model.ynnz.dot(np.log(psi_probabilities))
+
+    def _grad(self, psi_probabilities: NDArray):
+        weights = self._model.ynnz / psi_probabilities
+        return weights @ self._model.xnnz
+
+    def _psi_probabilities(self, param) -> NDArray:
+        return self._model.xnnz.dot(param)
+
+    def func(self, param: NDArray):
+        return self._func(self._psi_probabilities(param))
+
+    def grad(self, param: NDArray):
+        return self._func(self._psi_probabilities(param))
+
+    def func_and_grad(self, param: NDArray):
+        psi_probs = self._psi_probabilities(param)
+        return self._func(psi_probs), self._grad(psi_probs)
+
+
+class MultinomialLoss(Loss):
+    def __init__(self, kmer_model: multinomial_model):
+        self._model = kmer_model
+
+    def _func(self, param: NDArray, psi_probabilities: NDArray):
+        log_likelihood = self._model.ynnz.dot(np.log(psi_probabilities))
+        log_prior = -self._model.beta * param.dot(param)
+        return log_likelihood + log_prior
+
+    def _grad(self, param: NDArray, probs: NDArray, psi_probabilities: NDArray):
+        weights = self._model.ynnz / psi_probabilities
+        g_log_likelihood = self._model.xnnz.T @ weights * probs - self._model.N * probs
+        g_prior = -2 * self._model.beta * param
+        return g_log_likelihood + g_prior
+
+    def _psi_probabilities(self, probs: NDArray) -> NDArray:
+        return self._model.xnnz.dot(probs)
+
+    def func(self, param: NDArray):
+        probs = softmax(param)
+        return self._func(param, self._psi_probabilities(probs))
+
+    def grad(self, param: NDArray):
+        probs = softmax(param)
+        return self._grad(param, probs, self._psi_probabilities(probs))
+
+    def func_and_grad(self, param: NDArray):
+        probs = softmax(param)
+        psi_probs = self._psi_probabilities(probs)
+        return self._func(param, psi_probs), self._grad(param, probs, psi_probs)
--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -6,14 +6,30 @@ from numpy.typing import NDArray
 from scipy.special import softmax
 
 from solver_comparison.problem.model import SIMPLEX, SOFTMAX, Model
+from solver_comparison.problem.problem import Problem
 from solver_comparison.solvers.expgrad import ExpGrad
 from solver_comparison.solvers.frank_wolfe import FrankWolfe
+from solver_comparison.solvers.initializer import Initializer
 from solver_comparison.solvers.lbfgs import LBFGS
 
 
 def normalize(weights):
     _weights = np.array(weights).astype(float)
     return _weights / np.sum(_weights)
+
+
+def gradient_finite_differences(func, param):
+    n_dim = np.shape(param)[0]
+    delta = 2 * np.sqrt(1e-12) * (1 + np.linalg.norm(param))
+    grad = np.zeros(n_dim)
+    e_i = np.zeros(n_dim)
+    for i in range(n_dim):
+        e_i[i] = 1
+        fxp = func(param + delta * e_i)
+        fxm = func(param - delta * e_i)
+        grad[i] = (fxp - fxm) / (2 * delta)
+        e_i[i] = 0
+    return grad
 
 
 class ToyModel(Model):
@@ -56,19 +72,6 @@ class ToyModel(Model):
 @pytest.mark.parametrize("use_softmax", [False, True])
 def test_gradients(use_softmax):
     toy_model = ToyModel(data_uniform, use_softmax=use_softmax)
-
-    def gradient_finite_differences(func, param):
-        n_dim = np.shape(param)[0]
-        delta = 2 * np.sqrt(1e-12) * (1 + np.linalg.norm(param))
-        grad = np.zeros(n_dim)
-        e_i = np.zeros(n_dim)
-        for i in range(n_dim):
-            e_i[i] = 1
-            fxp = func(param + delta * e_i)
-            fxm = func(param - delta * e_i)
-            grad[i] = (fxp - fxm) / (2 * delta)
-            e_i[i] = 0
-        return grad
 
     _, grad_val = toy_model.logp_grad(weights_shifted)
 
@@ -134,3 +137,35 @@ def test_optimizers(
     toy_model = ToyModel(data, use_softmax=use_softmax)
     param_end = optimizer.run(toy_model, param_start)
     assert np.allclose(toy_model.probabilities(param_end), expected)
+
+
+K, N, L = 8, 100, 14
+
+
+@pytest.mark.parametrize(
+    "problem",
+    [
+        Problem(
+            model_type=SIMPLEX, filename="test5.fsa", K=K, N=N, L=L, alpha=0.1, beta=1.0
+        ),
+        Problem(
+            model_type=SOFTMAX, filename="test5.fsa", K=K, N=N, L=L, alpha=0.1, beta=1.0
+        ),
+    ],
+)
+def test_no_error_on_simple_experiment(problem):
+    model = problem.load_model()
+
+    w0 = Initializer("simplex_uniform").initialize_model(model)
+
+    f_new, g_new = model._loss.func_and_grad(w0)
+    f_old, g_old = model.kmerexpr_model.logp_grad(theta=w0)
+    assert f_new == f_old
+    assert np.allclose(g_new, g_old)
+
+    w0 = normalize(w0 + np.random.randn(np.shape(w0)[0]) ** 2)
+
+    f_new, g_new = model._loss.func_and_grad(w0)
+    f_old, g_old = model.kmerexpr_model.logp_grad(theta=w0)
+    assert f_new == f_old
+    assert np.allclose(g_new, g_old)
