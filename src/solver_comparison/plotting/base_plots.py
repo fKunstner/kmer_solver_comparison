13bc9b8e87954dd3e7e6da5dfc41bf3877df4bd1
fkunstner <frederik.kunstner@gmail.com>
2022-12-28 20:28:07 -0800

    move plot code reorg

--- src/solver_comparison/plotting/base_plots.py
+++ src/solver_comparison/plotting/base_plots.py
@@ -1,8 +1,10 @@
 import os
 
-import matplotlib.pyplot as plt
 import numpy as np
 from kmerexpr.utils import get_errors
+from matplotlib import pyplot as plt
+
+from solver_comparison.plotting.style import markers, palette
 
 
 def plot_scatter(title, xaxis, yaxis, horizontal=False, save_path="./figures"):
@@ -152,4 +154,178 @@ def plot_error_vs_iterations(
         xticks=dict_results["iteration_counts"],
         xaxislabel="iterations",
     )
+    plt.close()
+
+
+def plot_stat(stat, dict_results, title, opt_name, save_path="./figures"):
+    dict_plot = {opt_name: [dict_results[stat]]}
+    plot_general(
+        dict_plot,
+        title=title + stat,
+        save_path=save_path,
+        yaxislabel=stat,
+        xticks=dict_results["iteration_counts"],
+        xaxislabel="iterations",
+        miny=np.min(dict_plot[opt_name]),
+    )
+    plt.close()
+
+
+def plot_optimization_error(dict_results, title, opt_name, save_path="./figures"):
+    dict_plot = {opt_name: [-np.array(dict_results["loss_records"])]}
+    plot_general(
+        dict_plot,
+        title=title,
+        save_path=save_path,
+        yaxislabel=r"$f(\theta)$",
+        xticks=dict_results["iteration_counts"],
+        xaxislabel="iterations",
+        miny=np.min(dict_plot[opt_name]),
+    )
+    plt.close()
+
+
+def plot_general_different_xaxes(
+    result_dict,
+    xs_dict,
+    title,
+    save_path,
+    yaxislabel,
+    xaxislabel,
+    logplot=True,
+    fontsize=30,
+    miny=10000,
+):
+    plt.rc("text", usetex=True)
+    plt.rc("font", family="sans-serif")
+
+    for algo_name, marker, color in zip(result_dict.keys(), markers, palette):
+        print("plotting: ", algo_name)
+        result = result_dict[algo_name]
+        xs = xs_dict[algo_name]
+
+        if np.min(result) <= 0 or logplot == False:
+            plt.plot(
+                xs,
+                result,
+                marker,
+                markersize=12,
+                label=algo_name,
+                lw=3,
+                color=color,
+            )
+        else:
+            plt.semilogy(
+                xs,
+                result,
+                marker,
+                markersize=12,
+                label=algo_name,
+                lw=3,
+                color=color,
+            )
+
+        newmincand = np.min(result)
+        if miny > newmincand:
+            miny = newmincand
+
+    plt.ylim(bottom=miny)  # (1- (miny/np.abs(miny))*0.1)
+    plt.tick_params(labelsize=20)
+    plt.legend(fontsize=fontsize)
+    plt.xlabel(xaxislabel, fontsize=25)
+    plt.ylabel(yaxislabel, fontsize=25)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plt.savefig(
+        os.path.join(save_path, title + ".pdf"), bbox_inches="tight", pad_inches=0.01
+    )
+    print("Saved plot ", os.path.join(save_path, title + ".pdf"))
+    plt.gcf()
+    plt.close()
+
+
+def plot_on_axis(
+    ax,
+    result_dict,
+    xs_dict,
+    xaxislabel,
+    yaxislabel,
+    logplot=True,
+    miny=100000,
+    fontsize=30,
+):
+    for algo_name, marker, color in zip(result_dict.keys(), markers, palette):
+        print("plotting: ", algo_name)
+        result = result_dict[algo_name]
+        xs = xs_dict[algo_name]
+
+        if np.min(result) <= 0 or logplot == False:
+            ax.plot(
+                xs,
+                result,
+                marker,
+                markersize=12,
+                label=algo_name,
+                lw=3,
+                color=color,
+            )
+        else:
+            ax.semilogy(
+                xs,
+                result,
+                marker,
+                markersize=12,
+                label=algo_name,
+                lw=3,
+                color=color,
+            )
+
+        newmincand = np.min(result)
+        if miny > newmincand:
+            miny = newmincand
+
+    ax.set_ylim(bottom=miny)  # (1- (miny/np.abs(miny))*0.1)
+    # ax.set_tick_params(labelsize=20)
+    ax.legend(fontsize=fontsize)
+    ax.set_xlabel(xaxislabel, fontsize=25)
+    ax.set_ylabel(yaxislabel, fontsize=25)
+
+
+def plot_multiple(
+    multiple_result_dict,
+    xs_dict,
+    title,
+    save_path,
+    xaxislabel,
+    logplot=True,
+    fontsize=30,
+    miny=10000,
+):
+    plt.rc("text", usetex=False)
+    plt.rc("font", family="sans-serif")
+
+    n_plots = len(multiple_result_dict)
+    scale = 12
+    fig = plt.figure(figsize=(scale * n_plots, scale))
+    axes = [fig.add_subplot(1, n_plots, i) for i in range(1, n_plots + 1)]
+
+    for i, (name, result_dict) in enumerate(multiple_result_dict.items()):
+        plot_on_axis(
+            axes[i],
+            result_dict,
+            xs_dict,
+            xaxislabel,
+            yaxislabel=name,
+            logplot=logplot,
+            fontsize=fontsize,
+            miny=miny,
+        )
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plt.savefig(
+        os.path.join(save_path, title + ".pdf"), bbox_inches="tight", pad_inches=0.01
+    )
+    print("Saved plot ", os.path.join(save_path, title + ".pdf"))
+    plt.gcf()
     plt.close()
